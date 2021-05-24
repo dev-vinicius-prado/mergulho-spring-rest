23b3d962c2c085728680abe97650b1059612d2d4
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-05-24 19:22:33 -0300

    feat(Entregas): recursos da entrega.
    - criando mapeamendo das entidades para recursos do endpoint de entregas.

---
+++ src/main/java/com/algaworks/algalog/api/resources/DestinatarioResource.java
@@ -0,0 +1,15 @@
+package com.algaworks.algalog.api.resources;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class DestinatarioResource {
+
+    private String bairro;
+    private String complemento;
+    private String logradouro;
+    private String nome;
+    private String numero;
+}
---
+++ src/main/java/com/algaworks/algalog/api/resources/EntregaResource.java
@@ -0,0 +1,25 @@
+package com.algaworks.algalog.api.resources;
+
+import com.algaworks.algalog.domain.model.StatusEntrega;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Getter
+
+
+@Setter
+public class EntregaResource {
+
+    private ClienteResource cliente;
+    private OffsetDateTime dataFinalizacao;
+    private OffsetDateTime dataPedido;
+    private DestinatarioResource destinatario;
+    private Long id;
+    private StatusEntrega statusEntrega;
+    private BigDecimal taxa;
+
+
+}
