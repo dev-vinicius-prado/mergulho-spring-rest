07cdf7983451b7f3cb6229ae91ad43b6a07bc5cf
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-05-25 08:35:32 -0300

    style(NegocioException): code style
    - removendo linhas em branco.

--- src/main/java/com/algaworks/algalog/domain/exceptions/NegocioException.java
+++ src/main/java/com/algaworks/algalog/domain/exceptions/NegocioException.java
@@ -1,6 +1,7 @@
 package com.algaworks.algalog.domain.exceptions;
 
 public class NegocioException extends RuntimeException {
+
     public NegocioException(String ocorrenciaDaOperacao) {
         super(ocorrenciaDaOperacao);
     }
