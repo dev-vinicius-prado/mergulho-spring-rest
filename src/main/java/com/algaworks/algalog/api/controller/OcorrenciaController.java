f27214ad5386f3d1d34c7e42be5861a6a939a76c
dev-vinicius-prado <developer.vinicius.prado@gmail.com>
2021-05-25 08:51:38 -0300

    feat(OcorrenciaController): listar
    - criando método para listar as ocorrências de uma entrega.

--- src/main/java/com/algaworks/algalog/api/controller/OcorrenciaController.java
+++ src/main/java/com/algaworks/algalog/api/controller/OcorrenciaController.java
@@ -3,6 +3,7 @@ package com.algaworks.algalog.api.controller;
 import com.algaworks.algalog.api.mappers.OcorrenciaMapper;
 import com.algaworks.algalog.api.resources.OcorrenciaResource;
 import com.algaworks.algalog.domain.model.input.OcorrenciaInput;
+import com.algaworks.algalog.domain.services.BuscaEntregaService;
 import com.algaworks.algalog.domain.services.RegistroOcorrenciaService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -10,14 +11,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Collection;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("/entregas/{entregaId}/ocorrencias")
+@RequestMapping("/entregas/{idEntrega}/ocorrencias")
 public class OcorrenciaController {
 
     private OcorrenciaMapper ocorrenciaMapper;
     private RegistroOcorrenciaService registroOcorrenciaService;
+    private BuscaEntregaService buscaEntregaService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -26,5 +29,13 @@ public class OcorrenciaController {
         final var ocorrencia = registroOcorrenciaService
                 .registrar(entregaId, ocorrenciaInput.getDescricao());
         return ResponseEntity.ok(ocorrenciaMapper.toResource(ocorrencia));
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<OcorrenciaResource>> listar(@PathVariable Long idEntrega) {
+        var entrega = buscaEntregaService.buscar(idEntrega);
+        final var ocorrenciaResources = ocorrenciaMapper.toCollection(entrega.getOcorrencias());
+
+        return ResponseEntity.ok(ocorrenciaResources);
     }
 }
